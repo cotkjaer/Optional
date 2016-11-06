5e821aa082b190ee75986b0d84416ed5bbd58ded
christian otkjær <christian.otkjaer@gmail.com>
2016-11-06 11:36:40 +0100

    Initial Commit

---
+++ Optional/Optional.h
@@ -0,0 +1,19 @@
+//
+//  Optional.h
+//  Optional
+//
+//  Created by Christian Otkjær on 06/11/16.
+//  Copyright © 2016 Silverback IT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Optional.
+FOUNDATION_EXPORT double OptionalVersionNumber;
+
+//! Project version string for Optional.
+FOUNDATION_EXPORT const unsigned char OptionalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Optional/PublicHeader.h>
+
+
